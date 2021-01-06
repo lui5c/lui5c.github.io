f7f749f3391e2feb34e1c0eca235794c4905b079
lui5c <luiscollado26@gmail.com>
2021-01-06 15:47:21 -0500

    added CSS media queries and changed sidebar for responsiveness

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -1,43 +1,58 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import profpic from "../../static/profile.jpg"
 import github_svg from "../../static/github.svg"
 import gmail_svg from "../../static/gmail.svg"
 
-export default function Sidenav() {
-	return (
-		<div className="sidebar">
-			{/* profile picture */}
-			<Link to="/">
-				<img
-					className="profpic"
-					src={profpic}
-					alt="me smiling, link to landing page"
-				/>
-			</Link>
-			{/* socials */}
-			<div className="sidebar-socials">
-				<a href="https://github.com/lui5c">
-					<img src={github_svg} alt="github logo" />
-				</a>
-				<a href="mailto:luiscollado26@gmail.com">
-					<img src={gmail_svg} alt="gmail logo"/>
-				</a>
-				<div className="shortBio">
-					Luis Collado 
+class Sidenav extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			expanded: false,
+		}
+		this.toggleMobileMenu = this.toggleMobileMenu.bind(this);
+	}
+	toggleMobileMenu() {
+		this.setState({ expanded: !this.state.expanded, });
+	}
+	render() {
+		return (
+			<div className={`${this.state.expanded ? 'expanded-sidebar' : 'sidebar' }`}>
+				{/* profile picture */}
+				<Link to="/">
+					<img
+						className="profpic"
+						src={profpic}
+						alt="me smiling, link to landing page"
+					/>
+				</Link>
+				{/* socials */}
+				<div className="sidebar-socials">
+					<a href="https://github.com/lui5c">
+						<img src={github_svg} alt="github logo" />
+					</a>
+					<a href="mailto:luiscollado26@gmail.com">
+						<img src={gmail_svg} alt="gmail logo" />
+					</a>
+					<div className="shortBio">
+						Luis Collado
 				</div>
-				<hr />
+					<hr />
+				</div>
+				{/* navbar */}
+				<nav>
+					<Navlink name="home" path="/" />
+					<Navlink name="label-explorer" />
+					<Navlink name="wbar" />
+					<Navlink name="newtab" />
+					{/* <Navlink name="experiments" /> */}
+					<li className="mobile-expand" role="none" onClick={this.toggleMobileMenu}>
+						+
+					</li>
+				</nav>
 			</div>
-			{/* navbar */}
-			<nav>
-				<Navlink name="home" path="/" />
-				<Navlink name="label-explorer" />
-				<Navlink name="wbar" />
-				<Navlink name="newtab" />
-				<Navlink name="experiments" />
-			</nav>
-		</div>
 		)
+	}
 }
 
 function Navlink(props) {
@@ -45,6 +60,8 @@ function Navlink(props) {
 	if (props.path) { path = props.path; }
 	else { path = "/" + props.name + "/"; }
 	return (
-		<li><Link to={path}>{props.name}</Link></li>
+		<li	role="none" class="nav-link"><Link to={path}>{props.name}</Link></li>
 		)
 }
+
+export default Sidenav
